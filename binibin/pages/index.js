9d4e5534f0540da689db989754c9b4c143aa323c
Lance Bulseco <lancebulseco@Lances-MacBook-Air.local>
2021-10-25 17:45:24 -0700

    CHange folder name comp -> Comps

--- binibin/pages/index.js
+++ binibin/pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../comp/footer'
+import Footer from '../comps/footer'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
-import Hero from '../comp/hero'
-import Side from '../comp/Side'
+import Hero from '../comps/hero'
+import Side from '../comps/Side'
 
 
 const Container = styled.div`
 display:flex;
-
+flex-direction:column;
 `
 
 export default function Home() {
   return <Container>
 
   <Hero></Hero>
-  {/* <Footer></Footer> */}
+  <Footer></Footer>
   {/* <Side></Side> */}
   </Container>
   
