2760214c426c0f31d5176b98d411a39a63c2d20a
catherinelamyw <clam166@my.bcit.ca>
2021-10-27 02:20:38 -0700

    added cards to feature page

--- binibin/pages/features.js
+++ binibin/pages/features.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import React from 'react';
 
 import GuestNavBar from '../comps/GuestNavBar'
+import FeaturesCard from '../comps/FeaturesCard';
+import icon2 from '/public/icon_throw_away.png';
 
 const PageCont = styled.div`
     flex:1;
@@ -11,6 +13,12 @@ const TopRow = styled.div`
   display:flex;
 `
 
+const FeaturesCont = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`;
+
 
 export default function Home() {
   return (
@@ -19,6 +27,11 @@ export default function Home() {
       <TopRow>
         <GuestNavBar></GuestNavBar>
       </TopRow>
+      <FeaturesCont>
+        <FeaturesCard></FeaturesCard>
+        <FeaturesCard src="/icon_throw_away.png" alt="throw away" h="Track" p="A visual tool to track the quantity and the breakdown of the waste produced the school"></FeaturesCard>
+        <FeaturesCard src="/icon_combo_chart.png" alt="chart" h="Reduce" p="Increase awareness of the environment to encourage the appropriate disposal of waste"></FeaturesCard>
+      </FeaturesCont>
       </PageCont>
     //</div>
   )
