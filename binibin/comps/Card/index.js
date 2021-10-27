898ceffa9c0b49b1dda8dbc7b09b153c582c56b8
Lance Bulseco <lancebulseco@Lances-Air.hitronhub.home>
2021-10-27 00:43:37 -0700

    I love coding

---
+++ binibin/comps/Card/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const Container = styled.div`
+display:flex;
+flex-grow:1;
+width:100%;
+height:80px;
+justify-content:center;
+background-color:#95AFBA;
+`
+
+const Card = ({})=>{
+    return<Container>
+        
+    </Container>
+  }
+  
+  export default Card;
--- binibin/pages/index.js
+++ binibin/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../comps/footer'
+import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
-import Hero from '../comps/hero'
+import Hero from '../comps/Hero'
 import Side from '../comps/Side'
+import Card from '../comps/Card'
 
 
 const Container = styled.div`
@@ -14,10 +15,10 @@ flex-direction:column;
 
 export default function Home() {
   return <Container>
-
+    <Card></Card>
   {/* <Hero></Hero> */}
   {/*<Footer></Footer> */}
-  <Side></Side>
+  {/* <Side></Side> */}
   </Container>
   
   
