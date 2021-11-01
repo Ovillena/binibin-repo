aca6205fe3808f6824fc03fea0a350c8555952b8
catherinelamyw <clam166@my.bcit.ca>
2021-11-01 16:14:57 -0700

    started the past entries page

---
+++ binibin/pages/pastentries.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import React from 'react';
+
+import EntryItem from '../comps/EntryItem';
+
+export default function Home() {
+  return(
+    <div>
+      <EntryItem/>
+    </div>
+  )
+}
