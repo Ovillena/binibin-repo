58fe0627ce4a6380cdff074879ed2e09acf751d5
catherinelamyw <clam166@my.bcit.ca>
2021-11-03 16:36:40 -0700

    working on past entries page and components

--- binibin/comps/EntryItem/index.js
+++ binibin/comps/EntryItem/index.js
@@ -1,35 +1,48 @@
 import styled from 'styled-components';
 
-const Square = styled.div`
-  width:25px;
-  length:25px;
-  background-color:#3C64B1;
-  margin:10px;
+const ItemCont = styled.div`
+display:flex;
+margin:10px;
+padding:5px;
+background-color:#ccc;
+align-items:center;
 `;
 
-const ItemCont = styled.div`
-  display:flex;
-  margin:10px;
-  `;
+const Square = styled.div`
+  min-width:30px;
+  max-width:30px;
+  min-height:30px;
+  max-height:30px;
+  border-radius:5px;
+  background-color:${props=>props.color};
+`;
   
-  const ItemName = styled.p`
-  font-weight:bold;
-  margin:10px;
-  `;
-  
-  const ItemQuantity = styled.p`
+const ItemName = styled.p`
   font-weight:bold;
   margin:10px;
 `;
+  
+const ItemQuantity = styled.p`
+  font-weight:bold;
+`;
+
+//"entry_id": 1,
+// "item_name": "Can (200 mL)",
+// "item_count": 5,
+// "unit": "unit",
+// "waste_type": "recycling"
+// how to change color based on waste_type
 
 const EntryItem = ({
-  itemn="Name",
-  itemq="1"
+  color="#3C64B1",
+  item_name="Component Can (200mL)",
+  item_count="0",
+  unit="u"
 }) => {
   return <ItemCont>
-    <Square/>
-    <ItemName>{itemn}</ItemName>
-    <ItemQuantity>{itemq}</ItemQuantity>
+    <Square color={color}/>
+    <ItemName>{item_name}</ItemName>
+    <ItemQuantity>&times; {item_count} {unit}</ItemQuantity>
   </ItemCont>
 }
 
--- binibin/pages/pastentries.js
+++ binibin/pages/pastentries.js
@@ -1,12 +1,117 @@
 import styled from 'styled-components';
 import React from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 import EntryItem from '../comps/EntryItem';
+import EntryDate from '../comps/EntryDate';
+// import EntryList from '../comps/EntryList';
+
+const Cont = styled.div`
+  display:flex;
+  flex-direction:column;
+`;
+
+const AllDaysList = styled.div`
+  margin:10px;
+`;
+
+const EntryDayList = styled.div`
+  display:flex;
+  background-color:#eee;
+  margin:5px 20px 0px 20px;
+`;
+const ListSection = styled.div`
+  display:flex;
+  margin:10px;
+  background-color:#ddd;
+  align-items:center;
+  overflow:auto;
+  flex-wrap:wrap;
+`;
+
+const SideSection = styled.div`
+  display:flex;
+  margin:10px;
+
+`;
+
+const EntryEdit = styled.a`
+
+`;
+
+const fakeData = [
+  {
+    entry_id: 1,
+    entry_date: "November 3",
+    item_name: "Sample Can (200 mL)",
+    item_count: 5,
+    unit: "units",
+    waste_type: "recycling"
+  },{
+    entry_id: 2,
+    entry_date: "November 2",
+    item_name: "Garbage",
+    item_count: 500,
+    unit: "g",
+    waste_type: "garbage"
+  },{
+    entry_id: 3,
+    entry_date: "November 1",
+    item_name: "Compost",
+    item_count: 400,
+    unit: "g",
+    waste_type: "compost"
+  },{
+    entry_id: 4,
+    entry_date: "November 1",
+    item_name: "Milk Carton (1 L)",
+    item_count: 2,
+    unit: "units",
+    waste_type: "recycling"
+  }
+]
 
 export default function Home() {
+  const [entries, setEntries] = useState(fakeData);
+  // const GetEntries = async ()=>{
+  //   const result = await axios.get("https://binibin-server.herokuapp.com/api/entries");
+  //   console.log(result.data)
+  // }
+  // useEffect(()=>{
+  //   const GetEntries = async()=>{
+  //     const result = await axios.get("https://binibin-server.herokuapp.com/api/entries")
+  //     console.log(result);
+  //     setEntries(result.data);
+  //   }
+
+  //   GetEntries()
+  // }, [])
   return(
-    <div>
-      <EntryItem/>
-    </div>
+    <Cont>
+      {
+        entries.map((o,i)=>(
+          <AllDaysList key={i}>
+            <EntryDayList>
+              <SideSection>
+                <EntryDate entry_date={o.entry_date}/>
+              </SideSection>
+              <ListSection>
+                  <EntryItem entry_id={o.entry_id} item_name={o.item_name} item_count={o.item_count} unit={o.unit} waste_type={o.waste_type}/>
+              </ListSection>
+              <SideSection>
+                <EntryEdit>Edit</EntryEdit>
+              </SideSection>
+            </EntryDayList>
+          </AllDaysList>
+          )
+        )
+      }
+    </Cont>
   )
 }
+//"entry_id": 1,
+// "item_name": "Can (200 mL)",
+// "item_count": 5,
+// "unit": "unit",
+// "waste_type": "recycling"
