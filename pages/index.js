8a4bff9ede2e2b158347f874d982cfa29b7464ab
Lancebulseco <lbulseco@my.bcit.ca>
2021-11-03 20:12:13 -0700

    Finished the input page but I dont know how to do the useState Sorry

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
+import FooterComp from '../comps/footer'
 
 import styled from 'styled-components';
 import React from 'react';
 
-import Garbages from '../comps/Garbage'
-import Recycles from '../comps/Recycle';
-import Quick from '../comps/Quickaccess';
-
+import GuestNavBar from '../comps/GuestNavBar';
+import HeaderText from '../comps/HeaderText';
+import HeroComp from '../comps/HeroComp'
+import CircleIcon from '../comps/CircleImage';
+import MyButton from '../comps/Button';
+import Subhead from '../comps/SubheadText';
 
 const PageCont = styled.div`
 display:flex;
@@ -66,9 +69,62 @@ const ButtonCont = styled.div`
 
 export default function Home(){
   return (
-    // <Garbages></Garbages>
-    // <Recycles></Recycles>
-    <Quick></Quick>
+    //<div className={styles.container}>
+      <PageCont>
+      <TopRow>
+        <GuestNavBar></GuestNavBar>
+      </TopRow>
+    
+      <HeroComp></HeroComp>
+
+    <HeaderCont>
+      <HeaderText>Our Mission</HeaderText>
+    </HeaderCont>
+
+
+    <MissionRow>
+      <MissionCont>
+        <CircleIcon></CircleIcon>
+        <Subhead text="Educate"></Subhead>
+        <BodyText>Learn where your garbage should be going to lessen harmful impacts to the waste system.</BodyText>
+      </MissionCont>
+
+      <MissionCont>
+        <CircleIcon></CircleIcon>
+        <Subhead text="Educate"></Subhead>
+        <BodyText>Learn where your garbage should be going to lessen harmful impacts to the waste system.</BodyText>
+      </MissionCont>
+
+      <MissionCont>
+        <CircleIcon></CircleIcon>
+        <Subhead text="Educate"></Subhead>
+        <BodyText>Learn where your garbage should be going to lessen harmful impacts to the waste system.</BodyText>
+      </MissionCont>
+
+    </MissionRow>
+
+    <ParagraphSec>
+      <Subhead>Recycling Problems</Subhead>
+      <BodyText>Many people don’t know what to recycle or people are throwing away things that are recyclable. People are not aware of how much garbage they are sending to the landfill when portions of it may be compostable or recyclable.</BodyText>
+    </ParagraphSec>
+
+    <ParagraphSec>
+      <Subhead>You can make a difference</Subhead>
+      <BodyText>We encourage schools to promote waste reduction awareness to their students by providing a visual tool to track the quantity and the breakdown of the waste produced by their schools.</BodyText>
+    </ParagraphSec>
+
+    <ButtonCont>
+      <MyButton routeTo="learnmore" width="200px" height="65px" bgcolor="#FFC800" text="Learn More"></MyButton>
+    </ButtonCont>
+
+    
+    <FooterCont>
+      <FooterComp></FooterComp>
+    </FooterCont> 
+
+
+      </PageCont>
+
+    //</div>
   )
 }
-
