c4628a73715bac32c7e3c014a2b5fc11e987ff30
catherinelamyw <clam166@my.bcit.ca>
2021-11-19 15:10:37 -0800

    updated props to reflect new json data format

--- comps/EntryItem/index.js
+++ comps/EntryItem/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const ItemCont = styled.div`
   display:flex;
+  flex-direction:column;
   margin:10px;
   padding:5px;
   align-items:center;
@@ -13,11 +14,7 @@ const Square = styled.div`
   min-height:30px;
   max-height:30px;
   border-radius:5px;
-  background-color:${({color}) =>
-    color === "garbage" && "#000" ||
-    color === "compost" && "#598B2C" ||
-    color === "recycling" && "#2C5489"
-  };
+  background-color:${props=>props.color};
 `;
   
 const ItemName = styled.p`
@@ -29,23 +26,72 @@ const ItemQuantity = styled.p`
   font-weight:bold;
 `;
 
-//"entry_id": 1,
-// "item_name": "Can (200 mL)",
-// "item_count": 5,
-// "unit": "unit",
-// "waste_type": "recycling"
-// how to change color based on waste_type
+const SingleItem = styled.div`
+  display:flex;
+  align-items:center;
+  margin:0 10px 10px; 10px;
+`;
+
+const Notes = styled.span`
+  display:flex;
+  background-color:${({color}) =>
+    color === "garbage" && "#ccc" ||
+    color === "compost" && "#598B2C" ||
+    color === "recycling" && "#2C5489"
+  };
+`;
+
+const Minimized = styled.div`
+  display:flex;
+`;
+
+// [
+//   {
+//     "item_count": 1,
+//     "waste_type": "compost",
+//     "notes": "apples, oranges, banana peels",
+//     "account_id": 2
+//   },
+//   {
+//     "item_count": 5,
+//     "waste_type": "recycling",
+//     "notes": "pop cans",
+//     "account_id": 2
+//   },
+//   {
+//     "item_count": 1,
+//     "waste_type": "garbage",
+//     "notes": "",
+//     "account_id": 2
+//   }
+// ]
 
 const EntryItem = ({
   waste_type="recycling",
-  item_name="Component Can (200mL)",
-  item_count="0",
-  unit="u"
+  item_count_g="0",
+  item_count_c="0",
+  item_count_r="0",
+  notes="banana peels, bottle"
 }) => {
   return <ItemCont>
-    <Square color={waste_type}/>
-    <ItemName>{item_name}</ItemName>
-    <ItemQuantity>&times; {item_count} {unit}</ItemQuantity>
+    <Minimized>
+      <SingleItem>
+        <Square color="#000"/>
+        <ItemName>Garbage</ItemName>
+        <ItemQuantity>&times; {item_count_g}</ItemQuantity>
+      </SingleItem>
+      <SingleItem>
+        <Square color="#598B2C"/>
+        <ItemName>Compost</ItemName>
+        <ItemQuantity>&times; {item_count_c}</ItemQuantity>
+      </SingleItem>
+      <SingleItem>
+        <Square color="#2C5489"/>
+        <ItemName>Recycling</ItemName>
+        <ItemQuantity>&times; {item_count_r}</ItemQuantity>
+      </SingleItem>
+    </Minimized>
+    <Notes color={waste_type}>{notes}</Notes>
   </ItemCont>
 }
 
