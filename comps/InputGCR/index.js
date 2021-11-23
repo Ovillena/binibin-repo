55a058cf4f1580765cb38179504cd0037f38d80c
catherinelamyw <clam166@my.bcit.ca>
2021-11-22 16:24:10 -0800

    finished basic input functions

--- comps/InputGCR/index.js
+++ comps/InputGCR/index.js
@@ -123,66 +123,134 @@ const IGCR = ({
   item_count="0",
 }) => {
 
-function clickGarbage(){
-  //Gets the selected option
-  var g = document.getElementById("garbageSelect").value;
-  var resultG = parseFloat(g);
+//-------Click garbage function-----------
 
-  // var totalNumber = localStorage.getItem(resultG);
-  
+  function clickGarbage(){
 
-  // //add 1 to existing
-  // var addNumber = totalNumber+ resultG;
+  //NUMBER OF GARBAGE ITEMS
 
-  // //set new total
-  // localStorage.setItem(resultG, addNumber)
+    //get the selected number of garbage option and turn it into a float
+    var g = parseFloat(document.getElementById("garbageSelect").value);
 
-  //TO DISPLAY IN HTML
-  let sumG = resultG ;
-  document.getElementById("garbageCount").innerHTML = sumG;
+    if (typeof(Storage) !== "undefined"){
+      //if there is something already stored in garcount,
+      if (localStorage.garcount) {
+        //add the selected option to the current garcount value in localstorage
+        localStorage.garcount = parseFloat(localStorage.garcount) + g;
+      } else {
+        //if there were no garbage entries, set the garcount to zero
+        localStorage.garcount = g;
+      }
+    }
+    //display the garbage count in HTML
+    document.getElementById("garbageCount").innerHTML = localStorage.garcount;
 
+  //GARBAGE NOTES
 
-//To display text 
+    //get the newest inputted note for garbage
+    var garinput = document.getElementById("garbageText").value; 
+    
+    var addGarbageNote = function (gartext, garinput, delimiter) {
+      //get the existing notes stored in gartext
+      var existing = localStorage.getItem(gartext);
+      //add the newest inputted note to the existing notes
+      var data = existing ? existing + delimiter + garinput : garinput;
+      //save the new note
+      localStorage.setItem(gartext, data);
+    }
 
-  
+    addGarbageNote("gartext", garinput, ", ")
 
-  var getGarbageText = document.getElementById("garbageText").value; 
-  
-  localStorage.setItem('getGarbageText', getGarbageText)
-  
-  var existing = localStorage.getItem('garbageText');
+    //display the garbage note in HTML
+    document.getElementById("textGarbage").innerHTML = localStorage.gartext;
 
-  var data = existing ? existing + getGarbageText : 'tuna';
-
-  localStorage.setItem('garbageText', data);
-
-  document.getElementById("TextGarbage").innerHTML = data + " " ;
-
-
-} // END OF clickGarbage function
+  } // End of clickGarbage function
 
 
 //-------Click compost function-----------
 
 function clickCompost(){
-  var c = document.getElementById("compostSelect").value;
-  var resultC = parseFloat(c);
-  document.getElementById("compostCount").innerHTML = resultC;
 
-  var compostText = document.getElementById("compostText").value; 
-  document.getElementById("TextCompost").innerHTML = compostText;
-}
+  //NUMBER OF COMPOST ITEMS
+
+    //get the selected number of compost option and turn it into a float
+    var c = parseFloat(document.getElementById("compostSelect").value);
+
+    if (typeof(Storage) !== "undefined"){
+      //if there is something already stored in comcount,
+      if (localStorage.comcount) {
+        //add the selected option to the current comcount value in localstorage
+        localStorage.comcount = parseFloat(localStorage.comcount) + c;
+      } else {
+        //if there were no compost entries, set the comcount to zero
+        localStorage.comcount = c;
+      }
+    }
+    //display the compost count in HTML
+    document.getElementById("compostCount").innerHTML = localStorage.comcount;
+
+  //COMPOST NOTES
+
+    //get the newest inputted note for compost
+    var cominput = document.getElementById("compostText").value; 
+    
+    var addCompostNote = function (comtext, cominput, delimiter) {
+      //get the existing notes stored in comtext
+      var existing = localStorage.getItem(comtext);
+      //add the newest inputted note to the existing notes
+      var data = existing ? existing + delimiter + cominput : cominput;
+      //save the new note
+      localStorage.setItem(comtext, data);
+    }
+
+    addCompostNote("comtext", cominput, ", ")
+
+    //display the compost note in HTML
+    document.getElementById("textCompost").innerHTML = localStorage.comtext;
+
+} // End of clickCompost function
 
 //---------CLICK RECYCLE FUNCTION-----------
 
 function clickRecycle(){
-  var r = document.getElementById("recycleSelect").value;
-  var resultR = parseFloat(r);
-  document.getElementById("recycleCount").innerHTML = resultR;
 
-  var recycleText = document.getElementById("recycleText").value; 
-  document.getElementById("TextRecycle").innerHTML = recycleText;
-}
+  //NUMBER OF RECYCLABLE ITEMS
+
+    //get the selected number of recycle option and turn it into a float
+    var r = parseFloat(document.getElementById("recycleSelect").value);
+
+    if (typeof(Storage) !== "undefined"){
+      //if there is something already stored in reccount,
+      if (localStorage.reccount) {
+        //add the selected option to the current reccount value in localstorage
+        localStorage.reccount = parseFloat(localStorage.reccount) + r;
+      } else {
+        //if there were no recycle entries, set the reccount to zero
+        localStorage.reccount = r;
+      }
+    }
+    //display the recycle count in HTML
+    document.getElementById("recycleCount").innerHTML = localStorage.reccount;
+
+  //RECYCLABLE NOTES
+
+    //get the newest inputted note for recycle
+    var recinput = document.getElementById("recycleText").value; 
+    
+    var addRecycleNote = function (rectext, recinput, delimiter) {
+      //get the existing notes stored in rectext
+      var existing = localStorage.getItem(rectext);
+      //add the newest inputted note to the existing notes
+      var data = existing ? existing + delimiter + recinput : recinput;
+      //save the new note
+      localStorage.setItem(rectext, data);
+    }
+
+    addRecycleNote("rectext", recinput, ", ")
+
+    //display the recbage note in HTML
+    document.getElementById("textRecycle").innerHTML = localStorage.rectext;
+} // End of clickRecycle function
 
   return <PageCont>
 
@@ -267,15 +335,15 @@ function clickRecycle(){
   <BotCart>
 
     <TextCont>
-      <TextG id="TextGarbage"></TextG>
+      <TextG id="textGarbage"></TextG>
     </TextCont>
 
     <TextCont>
-      <TextG id="TextCompost"></TextG>
+      <TextG id="textCompost"></TextG>
     </TextCont>
 
     <TextCont>
-      <TextG id="TextRecycle"></TextG>
+      <TextG id="textRecycle"></TextG>
     </TextCont>
     
   </BotCart>
