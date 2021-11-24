5fa212b5c0f5d333779c897fe2c669f2e9cb1ee4
Megan Kuo <megan.kuo@live.com>
2021-11-24 14:46:20 -0800

    commented out stuff

--- pages/inputdata.js
+++ pages/inputdata.js
@@ -39,52 +39,52 @@ const ButtonCont = styled.div`
 
 export default function Home() {
 	// --------- Form for post submission ---------
-	const postData = {
-		garbage_text: localStorage.garbageText,
-		garbage_count: parseInt(localStorage.garbageCount),
-		compost_text: localStorage.compostText,
-		compost_count: parseInt(localStorage.compostCount),
-		recycling_text: localStorage.recyclingText,
-		recycling_count: parseInt(localStorage.recyclingCount),
-	};
+	// const postData = {
+	// 	garbage_text: localStorage.garbageText,
+	// 	garbage_count: parseInt(localStorage.garbageCount),
+	// 	compost_text: localStorage.compostText,
+	// 	compost_count: parseInt(localStorage.compostCount),
+	// 	recycling_text: localStorage.recyclingText,
+	// 	recycling_count: parseInt(localStorage.recyclingCount),
+	// };
 
-	const requestOptions = {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify(postData),
-	};
+	// const requestOptions = {
+	// 	method: 'POST',
+	// 	headers: { 'Content-Type': 'application/json' },
+	// 	body: JSON.stringify(postData),
+	// };
 
-	const resetStore = () => {
-		// creating default state for localStorage
-		localStorage.garbageCount = 0;
-		localStorage.garbageText = '';
-		localStorage.compostCount = 0;
-		localStorage.compostText = '';
-		localStorage.recyclingCount = 0;
-		localStorage.recyclingText = '';
-	};
+	// const resetStore = () => {
+	// 	// creating default state for localStorage
+	// 	localStorage.garbageCount = 0;
+	// 	localStorage.garbageText = '';
+	// 	localStorage.compostCount = 0;
+	// 	localStorage.compostText = '';
+	// 	localStorage.recyclingCount = 0;
+	// 	localStorage.recyclingText = '';
+	// };
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		await fetch('https://binibin-server.herokuapp.com/api/entries/add', requestOptions)
-			.then((response) => {
-				if (response.ok) {
-					console.log(response);
-					alert(`Your entry has been submitted!`);
-					resetStore();
-				} else {
-					throw new Error('Unable to perform POST request');
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+	// const onSubmit = async (e) => {
+	// 	e.preventDefault();
+	// 	await fetch('https://binibin-server.herokuapp.com/api/entries/add', requestOptions)
+	// 		.then((response) => {
+	// 			if (response.ok) {
+	// 				console.log(response);
+	// 				alert(`Your entry has been submitted!`);
+	// 				resetStore();
+	// 			} else {
+	// 				throw new Error('Unable to perform POST request');
+	// 			}
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err);
+	// 		});
+	// };
 
-	useEffect(() => {
-		// creating default state for localStorage
-		resetStore();
-	}, []);
+	// useEffect(() => {
+	// 	// creating default state for localStorage
+	// 	resetStore();
+	// }, []);
 
 	return (
 		//<div className={styles.container}>
