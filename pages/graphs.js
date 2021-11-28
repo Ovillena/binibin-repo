e20cbca981e5428a130c26b6e49436c1d58485c5
Megan Kuo <megan.kuo@live.com>
2021-11-27 21:59:13 -0800

    prettier formatted and removed axios

--- pages/graphs.js
+++ pages/graphs.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
-
 
 import Header from '../comps/HeaderText';
 import Footer from '../comps/footer';
@@ -12,107 +10,57 @@ import RecycleBar from '../comps/GraphsRecycle';
 import GraphsSum from '../comps/GraphsSum';
 
 const PageCont = styled.div`
-  display:flex;
-  flex-direction:column;
-  min-height:100vh;
-`
-
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+`;
 
 const HeaderCont = styled.div`
-  display:flex;
-  justify-content:center;
-`
+	display: flex;
+	justify-content: center;
+`;
 
 const FooterCont = styled.div`
-  display:flex;
-  align-items:flex-end;
-  flex:1;
-  padding-top:50px;
-`
+	display: flex;
+	align-items: flex-end;
+	flex: 1;
+	padding-top: 50px;
+`;
 
 const WeekCont = styled.div`
-  display:flex;
-  justify-content:center;
-`
+	display: flex;
+	justify-content: center;
+`;
 
 const TopGraphs = styled.div`
-  display:flex;
-  justify-content:space-around;
-  padding:10px;
-  flex-wrap:wrap;
-`
+	display: flex;
+	justify-content: space-around;
+	padding: 10px;
+	flex-wrap: wrap;
+`;
 
-const GraphDiv = styled.div`
-
-`
+const GraphDiv = styled.div``;
 
 const BottomGraphs = styled.div`
-  display:flex;
-  justify-content:space-around;
-  padding:10px;
-  flex-wrap:wrap;
-`
+	display: flex;
+	justify-content: space-around;
+	padding: 10px;
+	flex-wrap: wrap;
+`;
 
 const CardWrapper = styled.div`
-  margin:10px;
-`
-
-const fakeData = [
-  {
-      entry_id: 1,
-      item_name: "Can (200 mL)",
-      item_count: 5,
-      unit: "unit",
-      waste_type: "recycling"
-  },
-  {
-      entry_id: 2,
-      item_name: "Garbage",
-      item_count: 500,
-      unit: "g",
-      waste_type: "garbage"
-  },
-  {
-      entry_id: 3,
-      item_name: "Compost",
-      item_count: 400,
-      unit: "g",
-      waste_type: "compost"
-  },
-  {
-      entry_id: 4,
-      item_name: "Milk Carton (1 L)",
-      item_count: 2,
-      unit: "unit",
-      waste_type: "recycling"
-  }
-]
-
+	margin: 10px;
+`;
 
 export default function Graphs() {
+	return (
+		//<div className={styles.container}>
+		<PageCont>
+			<HeaderCont>
+				<Header text='Waste Tracked for November'></Header>
+			</HeaderCont>
 
-  // const [number, setNumber] = useState(fakeData);
-
-  // useEffect(() => {
-  //   const GetNumber = async()=>{
-  //     const result = await axios.get("https://binibin-server.herokuapp.com/api/entries");
-  //     console.log(result);
-  //     //data from result will provide an array of films
-  //     setNumber(result.data)
-  //   }
-
-  //   GetNumber();
-  // }, [])
-  return (
-    //<div className={styles.container}>
-      <PageCont>
-
-
-      <HeaderCont>
-        <Header text="Waste Tracked for November"></Header>
-      </HeaderCont>
-
-      {/* <WeekCont>
+			{/* <WeekCont>
         <WeekOfComp onPrevInteract={()=>{
 
         }}
@@ -122,7 +70,7 @@ export default function Graphs() {
         ></WeekOfComp>
       </WeekCont> */}
 
-      {/* {
+			{/* {
         number.map((o,i)=>(
           <CardWrapper key={i}>
           <GraphsGarbage item_count={o.item_count}/>
@@ -131,32 +79,28 @@ export default function Graphs() {
         )
       } */}
 
-      <TopGraphs>
-        <GraphDiv>
-          <GraphsGarbage></GraphsGarbage>
-        </GraphDiv>
-        <GraphDiv>
-          <GraphsCompost></GraphsCompost>
-        </GraphDiv>
-      </TopGraphs>
+			<TopGraphs>
+				<GraphDiv>
+					<GraphsGarbage></GraphsGarbage>
+				</GraphDiv>
+				<GraphDiv>
+					<GraphsCompost></GraphsCompost>
+				</GraphDiv>
+			</TopGraphs>
 
-      <BottomGraphs>
-      <GraphDiv>
-        <RecycleBar></RecycleBar>
-      </GraphDiv>
-      <GraphDiv>
-        <GraphsSum></GraphsSum>
-      </GraphDiv>
+			<BottomGraphs>
+				<GraphDiv>
+					<RecycleBar></RecycleBar>
+				</GraphDiv>
+				<GraphDiv>
+					<GraphsSum></GraphsSum>
+				</GraphDiv>
+			</BottomGraphs>
 
-      </BottomGraphs>
-      
-      <FooterCont>
-        <Footer></Footer>
-      </FooterCont>
-
-      </PageCont>
-    //</div>
-  )
+			<FooterCont>
+				<Footer></Footer>
+			</FooterCont>
+		</PageCont>
+		//</div>
+	);
 }
-
-
