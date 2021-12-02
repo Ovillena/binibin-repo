b94f25f55fc431ee1cb87edbdac64daacd7a2434
Megan Kuo <megan.kuo@live.com>
2021-12-01 18:24:33 -0800

    new packages, delete default entryItems props, prettiered

--- comps/EntryItem/index.js
+++ comps/EntryItem/index.js
@@ -1,29 +1,29 @@
 import styled from 'styled-components';
 
 const ItemCont = styled.div`
-  display:flex;
-  flex-direction:column;
-  margin:10px;
-  padding:5px;
-  align-items:center;
+	display: flex;
+	flex-direction: column;
+	margin: 10px;
+	padding: 5px;
+	align-items: center;
 `;
 
 const Square = styled.div`
-  min-width:30px;
-  max-width:30px;
-  min-height:30px;
-  max-height:30px;
-  border-radius:5px;
-  background-color:${props=>props.color};
+	min-width: 30px;
+	max-width: 30px;
+	min-height: 30px;
+	max-height: 30px;
+	border-radius: 5px;
+	background-color: ${(props) => props.color};
 `;
 
 const ItemName = styled.p`
-  font-weight:bold;
-  margin:10px 5px 10px 10px;
+	font-weight: bold;
+	margin: 10px 5px 10px 10px;
 `;
 
 const ItemQuantity = styled.p`
-  font-weight:bold;
+	font-weight: bold;
 `;
 
 const SingleItem = styled.div`
@@ -33,8 +33,8 @@ const SingleItem = styled.div`
 `;
 
 const Notes = styled.div`
-  display:flex;
-  width:100%;
+	display: flex;
+	width: 100%;
 `;
 
 const Text = styled.span`
@@ -49,90 +49,97 @@ const Text = styled.span`
 `;
 
 const Minimized = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
+	display: flex;
+	width: 100%;
+	justify-content: center;
 `;
 
 const Expand = styled.button`
-  background-color: #FFC800;
-  border: none;
-  border-radius: 10px;
-  font-weight: bold;
-  height: 25px;
-  margin-top: 8px;
-  margin-left: 10px;
-  &:hover {
-      background-color: #E5A500;
-    }
+	background-color: #ffc800;
+	border: none;
+	border-radius: 10px;
+	font-weight: bold;
+	height: 25px;
+	margin-top: 8px;
+	margin-left: 10px;
+	&:hover {
+		background-color: #e5a500;
+	}
 `;
 
 const EntryItem = ({
-  garbage_text="snack wrappers",
-  garbage_count="2",
-  compost_text="banana peel",
-  compost_count="1",
-  recycling_text="yogurt container",
-  recycling_count="1",
-  expandContainer=()=>{},
-  show=false
+	garbage_text,
+	garbage_count,
+	compost_text,
+	compost_count,
+	recycling_text,
+	recycling_count,
+	expandContainer,
+	show = false,
 }) => {
-  if(show === false){
-    return <ItemCont>
-
-      <Minimized>
-        <SingleItem>
-          <Square color="#000"/>
-          <ItemName>Garbage</ItemName>
-          <ItemQuantity>&times; {garbage_count}</ItemQuantity>
-        </SingleItem>
-        <SingleItem>
-          <Square color="#598B2C"/>
-          <ItemName>Compost</ItemName>
-          <ItemQuantity>&times; {compost_count}</ItemQuantity>
-        </SingleItem>
-        <SingleItem>
-          <Square color="#2C5489"/>
-          <ItemName>Recycling</ItemName>
-          <ItemQuantity>&times; {recycling_count}</ItemQuantity>
-        </SingleItem>
-        <Expand
-          onClick={()=>{
-            expandContainer();
-          }}>Show Notes
-        </Expand>
-      </Minimized>
-    </ItemCont>
-  }
-  return <ItemCont>
-    <Minimized>
-      <SingleItem>
-        <Square color="#000"/>
-        <ItemName>Garbage</ItemName>
-        <ItemQuantity>&times; {garbage_count}</ItemQuantity>
-      </SingleItem>
-      <SingleItem>
-        <Square color="#598B2C"/>
-        <ItemName>Compost</ItemName>
-        <ItemQuantity>&times; {compost_count}</ItemQuantity>
-      </SingleItem>
-      <SingleItem>
-        <Square color="#2C5489"/>
-        <ItemName>Recycling</ItemName>
-        <ItemQuantity>&times; {recycling_count}</ItemQuantity>
-      </SingleItem>
-      <Expand
-          onClick={()=>{
-            expandContainer();
-          }}>Hide Notes
-        </Expand>
-    </Minimized>
-    <Notes>
-      <Text color="garbage">{garbage_text} </Text>
-      <Text color="compost">{compost_text} </Text>
-      <Text color="recycling">{recycling_text} </Text>
-    </Notes>
-  </ItemCont>
-}
+	if (show === false) {
+		return (
+			<ItemCont>
+				<Minimized>
+					<SingleItem>
+						<Square color='#000' />
+						<ItemName>Garbage</ItemName>
+						<ItemQuantity>&times; {garbage_count}</ItemQuantity>
+					</SingleItem>
+					<SingleItem>
+						<Square color='#598B2C' />
+						<ItemName>Compost</ItemName>
+						<ItemQuantity>&times; {compost_count}</ItemQuantity>
+					</SingleItem>
+					<SingleItem>
+						<Square color='#2C5489' />
+						<ItemName>Recycling</ItemName>
+						<ItemQuantity>&times; {recycling_count}</ItemQuantity>
+					</SingleItem>
+					<Expand
+						onClick={() => {
+							expandContainer();
+						}}
+					>
+						Show Notes
+					</Expand>
+				</Minimized>
+			</ItemCont>
+		);
+	}
+	return (
+		<ItemCont>
+			<Minimized>
+				<SingleItem>
+					<Square color='#000' />
+					<ItemName>Garbage</ItemName>
+					<ItemQuantity>&times; {garbage_count}</ItemQuantity>
+				</SingleItem>
+				<SingleItem>
+					<Square color='#598B2C' />
+					<ItemName>Compost</ItemName>
+					<ItemQuantity>&times; {compost_count}</ItemQuantity>
+				</SingleItem>
+				<SingleItem>
+					<Square color='#2C5489' />
+					<ItemName>Recycling</ItemName>
+					<ItemQuantity>&times; {recycling_count}</ItemQuantity>
+				</SingleItem>
+				<Expand
+					onClick={() => {
+						expandContainer();
+					}}
+				>
+					Hide Notes
+				</Expand>
+			</Minimized>
+			<Notes>
+				<Text color='garbage'>{garbage_text} </Text>
+				<Text color='compost'>{compost_text} </Text>
+				<Text color='recycling'>{recycling_text} </Text>
+			</Notes>
+		</ItemCont>
+	);
+};
 
 export default EntryItem;
